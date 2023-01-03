ecc0ff820372b58cd44b682b62f77d6d12626879
paul7dxb <62883464+paul7dxb@users.noreply.github.com>
2023-01-03 21:15:24 +0000

    tutorial 7

--- django_example/users/views.py
+++ django_example/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm # Form for User creation Extended form from forms.py
 from django.contrib import messages #Flash messages library
+from django.contrib.auth.decorators import login_required # Decorator to restrict access to pages
 
 def register(request):
     if request.method == 'POST':
@@ -9,10 +10,14 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
+            messages.success(request, f'Your account has been created. You can now login!')
             #Redirect user to home page after successful account create
-            return redirect('blog-home')
+            return redirect('login')
 
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', { 'form' : form })
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
