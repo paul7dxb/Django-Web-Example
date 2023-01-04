a7035a150c25aa81bdf7ed0b6658815349b6249a
paul7dxb <62883464+paul7dxb@users.noreply.github.com>
2023-01-04 13:36:13 +0000

    Tutorial 9

--- django_example/users/forms.py
+++ django_example/users/forms.py
@@ -1,14 +1,30 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 #Create new class that inherits from UserCreationForm
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-
 
     class Meta:
         #model to be affected
         model = User
         # Fields to display in form in order
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        #model to be affected
+        model = User
+        # Fields to display in form in order
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
