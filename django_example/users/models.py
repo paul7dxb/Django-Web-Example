fe314c46fc5169472d90664806e3c44ffd9013f4
paul7dxb <62883464+paul7dxb@users.noreply.github.com>
2023-01-04 07:56:28 +0000

    tutorial 8

--- django_example/users/models.py
+++ django_example/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    #One to one relationship with User Acccount. CASCADE will delete profile on user deletion. Not other way around
+    user = models.OneToOneField(User, on_delete=models.CASCADE) 
+    # Profile picture. Choose default image and directory where profile pictures are stored
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    # Dunder STR method for printing out object
+    def __str__(self) -> str:
+        return f'{self.user.username} Profile'
