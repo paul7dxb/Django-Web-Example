8f70a1629e90db9b3caa766cdace952ea32e7263
paul7dxb <62883464+paul7dxb@users.noreply.github.com>
2023-01-04 13:36:05 +0000

    Tutorial 9

--- django_example/users/models.py
+++ django_example/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     #One to one relationship with User Acccount. CASCADE will delete profile on user deletion. Not other way around
@@ -10,3 +11,14 @@ class Profile(models.Model):
     # Dunder STR method for printing out object
     def __str__(self) -> str:
         return f'{self.user.username} Profile'
+  
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs) #Run parents save method
+        #open profile image
+        img = Image.open(self.image.path)
+        #Resize image
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
--- django_example/users/views.py
+++ django_example/users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegisterForm # Form for User creation Extended form from forms.py
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm # Form for User creation Extended form from forms.py
 from django.contrib import messages #Flash messages library
 from django.contrib.auth.decorators import login_required # Decorator to restrict access to pages
 
@@ -20,4 +20,20 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES,  instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated')
+            return redirect('profile') #POST GET redirect pattern, forces GET request instead of post
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form' : u_form,
+        'p_form' : p_form
+    }
+    return render(request, 'users/profile.html', context)
