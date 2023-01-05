9271fc25cdcdf96ba8d773af241e5ff3ac3fe6d5
paul7dxb <62883464+paul7dxb@users.noreply.github.com>
2023-01-05 12:30:30 +0000

    tutorial 11

--- django_example/blog/urls.py
+++ django_example/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'), # pk is primary key, post-detail is the default view name
     path('post/new/', PostCreateView.as_view(), name='post-create'), # default view is post_form. Same as update view
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'), # pk is primary key, post-update uses post_form.html
